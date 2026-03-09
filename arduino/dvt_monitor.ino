a5f358cb08010c019d8bcb9b87a4ce555558c735
Mirthiga12 <mirthiga.2407028@srec.ac.in>
2026-03-09 11:28:11 +0530

    "Add variables and setup function"

--- arduino/dvt_monitor.ino
+++ arduino/dvt_monitor.ino
@@ -9,3 +9,17 @@
 
 // Objects
 MPU6050 mpu;
+// Variables
+int16_t ax, ay, az;
+float accelMag, prevAccel = 0;
+int motion = 0;
+
+// Setup Function
+void setup() {
+  Serial.begin(9600);
+  pinMode(MOTOR_PIN, OUTPUT);
+  analogWrite(MOTOR_PIN, 0);
+  Wire.begin();
+  mpu.initialize();
+  Serial.println(F("OK"));
+}

f237728354985b7f4deac871bd0e44d463922afc
Mirthiga12 <mirthiga.2407028@srec.ac.in>
2026-03-09 16:53:06 +0530

    "Add variables and setup function"

--- arduinoinput.tsx
+++ arduinoinput.tsx
@@ -1,9 +1,16 @@
 #include <Wire.h>
 #include "MPU6050.h"
-
-// Pin Definitions
 #define MOTOR_PIN 9
 #define FSR_PIN A0
-
-// Objects
 MPU6050 mpu;
+int16_t ax, ay, az;
+float accelMag, prevAccel = 0;
+int motion = 0;
+void setup() {
+  Serial.begin(9600);
+  pinMode(MOTOR_PIN, OUTPUT);
+  analogWrite(MOTOR_PIN, 0);
+  Wire.begin();
+  mpu.initialize();
+  Serial.println(F("OK"));
+}
